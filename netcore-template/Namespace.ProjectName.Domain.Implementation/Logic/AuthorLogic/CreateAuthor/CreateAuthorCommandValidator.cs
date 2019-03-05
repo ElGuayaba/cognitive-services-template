f05711bb40fddadeca7c9641558d0bd7de1c98d4
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-05 21:38:58 +0100

    Added example create author request

---
+++ netcore-template/Namespace.ProjectName.Domain.Implementation/Logic/AuthorLogic/CreateAuthor/CreateAuthorCommandValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace Namespace.ProjectName.Domain.Implementation.Logic.AuthorLogic.CreateAuthor
+{
+    public class CreateAuthorCommandValidator : AbstractValidator<CreateAuthorCommand>
+    {
+        public CreateAuthorCommandValidator()
+        {
+            //TODO
+        }
+    }
+}
