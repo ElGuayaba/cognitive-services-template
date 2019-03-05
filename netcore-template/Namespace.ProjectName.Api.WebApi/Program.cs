a857617d95351257fca1758a3202a55405fbbe0a
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-05 20:46:11 +0100

    Updated program and startup

--- netcore-template/Namespace.ProjectName.Api.WebApi/Program.cs
+++ netcore-template/Namespace.ProjectName.Api.WebApi/Program.cs
@@ -1,24 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Namespace.ProjectName.Common.IoC;
+using Serilog;
 
 namespace Namespace.ProjectName.Api.WebApi
 {
     public class Program
     {
+        private static IConfiguration Configuration { get; } = ConfigurationExtension.LoadConfiguration();
+        
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+        
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-        }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            Log.Logger = ConfigurationExtension.LoadLogger(Configuration);
+            
+            try
+            {
+                Log.Information("Starting ProjectName");
+
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "ProjectName terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
     }
-}
+}    
