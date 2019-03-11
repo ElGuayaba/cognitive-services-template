229d7b9603bffc360a033a85d45d14108e82d057
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-11 17:58:34 +0100

    IoC in each project

--- netcore-template/Namespace.ProjectName.Common.IoC/DependencyInjectionExtension.cs
+++ netcore-template/Namespace.ProjectName.Common.IoC/DependencyInjectionExtension.cs
@@ -1,5 +1,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Namespace.ProjectName.Application.Implementation.Configuration;
+using Namespace.ProjectName.Domain.Implementation.Configuration;
+using Namespace.ProjectName.Persistence.Implementation.Configuration;
 
 namespace Namespace.ProjectName.Common.IoC
 {
@@ -7,9 +10,9 @@ namespace Namespace.ProjectName.Common.IoC
     {
         public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddPersistenceDependencies(configuration);
-
-            services.AddIdentity(configuration);
+//            services.AddPersistenceDependencies(configuration);
+            services.AddApplicationDependencies(configuration);
+            services.AddDomainDependencies(configuration);
             
             return services;
         }
