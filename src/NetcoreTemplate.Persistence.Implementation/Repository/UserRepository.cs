40c18e8026b0809ea3cbece18bd5512f2473696e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-28 14:16:27 +0100

    Implemented example repositories

---
+++ src/NetcoreTemplate.Persistence.Implementation/Repository/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetcoreTemplate.Domain.Entity;
+using NetcoreTemplate.Persistence.Contract.Repository;
+using NetcoreTemplate.Persistence.Implementation.Context;
+
+namespace NetcoreTemplate.Persistence.Implementation.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(NetcoreTemplateDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
