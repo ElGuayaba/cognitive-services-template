8c15493311a4c25d4db3303b29c36480b8ab5c38
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-28 14:11:33 +0100

    Added NetcoreTemplateDbContext interface

---
+++ src/NetcoreTemplate.Persistence.Contract/Context/IDbContext.cs
@@ -0,0 +1,9 @@
+﻿﻿using Microsoft.EntityFrameworkCore;
+
+ namespace NetcoreTemplate.Persistence.Contract.Context
+{
+    public interface IDbContext
+    {
+        DbContext Instance { get; }
+    }
+}
