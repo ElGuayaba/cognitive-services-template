d190b1cff3a1317ebf7dc6c3999e7aab5f0d7ad5
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-28 12:49:44 +0100

    Added example entities

---
+++ src/NetcoreTemplate.Domain.Entity/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NetcoreTemplate.Domain.Entity
+{
+    public class Profile
+    {
+        public Guid Id { get; set; }
+        public ProfileAddress Address { get; set; }
+        
+        public string UserId { get; set; }
+        public User User { get; set; }
+        
+        public class ProfileAddress
+        {
+            public Guid Id { get; set; }
+            public string City { get; set; }
+            public string Country { get; set; }
+        }
+    }
+}
---
+++ src/NetcoreTemplate.Domain.Entity/User.cs
@@ -0,0 +1,8 @@
+﻿namespace NetcoreTemplate.Domain.Entity
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public Profile Profile { get; set; }
+    }
+}
