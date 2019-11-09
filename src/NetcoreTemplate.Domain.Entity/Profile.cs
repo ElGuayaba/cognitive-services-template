9073aba5b0761940c26885b4d09fdbd5cf33b60c
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-11-09 15:27:50 +0100

    Updated Domain.Entity

--- src/NetcoreTemplate.Domain.Entity/Profile.cs
+++ src/NetcoreTemplate.Domain.Entity/Profile.cs
@@ -5,16 +5,8 @@ namespace NetcoreTemplate.Domain.Entity
     public class Profile
     {
         public Guid Id { get; set; }
-        public ProfileAddress Address { get; set; }
         
         public string UserId { get; set; }
         public User User { get; set; }
-        
-        public class ProfileAddress
-        {
-            public Guid Id { get; set; }
-            public string City { get; set; }
-            public string Country { get; set; }
-        }
     }
 }
